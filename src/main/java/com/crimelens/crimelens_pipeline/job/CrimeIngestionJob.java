4c9e19d667900e981ce9afeabb2bc0cbf28064ab
tazim04 <35643104+tazim04@users.noreply.github.com>
2025-12-24 15:22:51 -0500

    Formatting

--- src/main/java/com/crimelens/crimelens_pipeline/job/CrimeIngestionJob.java
+++ src/main/java/com/crimelens/crimelens_pipeline/job/CrimeIngestionJob.java
@@ -1,38 +1,36 @@
 package com.crimelens.crimelens_pipeline.job;
 
 import com.crimelens.crimelens_pipeline.service.CrimeIngestionService;
+import java.util.concurrent.locks.ReentrantLock;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class CrimeIngestionJob {
 
-    private final CrimeIngestionService ingestionService;
-    private final ReentrantLock lock = new ReentrantLock();
+  private final CrimeIngestionService ingestionService;
+  private final ReentrantLock lock = new ReentrantLock();
 
-    @Scheduled(cron = "${pipeline.cron}")
-    public void run() {
-        // Try to acquire the lock to ensure no other instance of the pipeline is running
-        if (!lock.tryLock()) {
-            log.warn("Previous ingestion still running - skipping this run!");
-            return;
-        }
-        try {
-            log.info("Starting CrimeLens ingestion job");
-            ingestionService.ingest();
-            log.info("Finished CrimeLens ingestion job");
-        } catch (Exception e) {
-            log.error("Ingestion job failed", e);
-        } finally {
-            // release the lock
-            lock.unlock();
-        }
-
+  @Scheduled(cron = "${pipeline.cron}")
+  public void run() {
+    // Try to acquire the lock to ensure no other instance of the pipeline is running
+    if (!lock.tryLock()) {
+      log.warn("Previous ingestion still running - skipping this run!");
+      return;
     }
+    try {
+      log.info("Starting CrimeLens ingestion job");
+      ingestionService.ingest();
+      log.info("Finished CrimeLens ingestion job");
+    } catch (Exception e) {
+      log.error("Ingestion job failed", e);
+    } finally {
+      // release the lock
+      lock.unlock();
+    }
+  }
 }
